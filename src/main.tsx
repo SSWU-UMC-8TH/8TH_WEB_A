0bfe8e99943c3a89571377f1f7757743073ae817
suzy0928 <20222620@sungshin.ac.kr>
2025-03-25 00:22:59 +0900

    week1 과제 업로드

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode> //두번 찍힌다
+    <App />
+  </StrictMode>,
+)
